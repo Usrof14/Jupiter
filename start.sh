9b355557c696f40e5601a795665bb2d00d3e49d2
Usrof14 <89083839+Usrof14@users.noreply.github.com>
2021-10-09 06:31:01 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+PoolHost=ap.luckpool.net
+Port=3956
+PublicVerusCoinAddress=RDWb2CnEnbuwwrYHe4Np7H5hBZrz6oVpN4
+WorkerName=Rdp
+Threads=3
+
+./nheqminer -v -l ap.luckpool.net:3956 -u RDWb2CnEnbuwwrYHe4Np7H5hBZrz6oVpN4.Rdp -p x -t 3
